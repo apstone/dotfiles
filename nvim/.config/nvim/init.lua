0187158c7734a4f38f77c385b4f2bb480ef212f2
Aaron Stone <apstone@protonmail.com>
2024-11-11 20:55:39 -0500

    ruby

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -881,6 +881,7 @@ require("lazy").setup({
 				lua = { "stylua" },
 				php = { "pint" },
 				blade = { "blade-formatter" },
+				ruby = { "rubocop" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
@@ -1006,36 +1007,6 @@ require("lazy").setup({
 		end,
 	},
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-	{
-		"nvimtools/none-ls.nvim",
-		event = "VeryLazy",
-		opts = function()
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-			local null_ls = require("null-ls")
-
-			return {
-				sources = {
-					null_ls.builtins.formatting.prettierd,
-				},
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({
-							group = augroup,
-							buffer = bufnr,
-						})
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format({ bufnr = bufnr })
-							end,
-						})
-					end
-				end,
-			}
-		end,
-	},
 
 	-- Highlight todo, notes, etc in comments
 	{
