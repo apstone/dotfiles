8c5213b37184f22e921e1f6b25e16b2890da28c0
Aaron Stone <apstone@protonmail.com>
2024-11-14 10:28:30 -0500

    nvchad

---
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,33 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "html", "cssls", "ts_ls", "eslint" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+-- configuring single server, example: typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
+
+lspconfig.eslint.setup({
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+})
---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,12 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+--
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+ require("nvconfig").base46.theme = 'catppuccin'
+ require("base46").load_all_highlights()
